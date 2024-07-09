027ac87e6cba6b7b2b7b810c45547aa358278180
Dima K <@users.noreply.github.com>
2024-07-09 15:08:12 +0200

    Solved the flask option challenge

--- flask_option.py
+++ flask_option.py
@@ -3,8 +3,14 @@
 import os
 
 def start():
-    """returns the right message"""
-    pass  # YOUR CODE HERE
+    ''' returns the right message'''
+    value = os.getenv('FLASK_ENV')
+    if value == 'development':
+        return "Starting in development mode..."
+    if value == 'production':
+        return "Starting in production mode..."
+
+    return "Starting in empty mode..."
 
 if __name__ == "__main__":
     print(start())
